28f3a6910893e4997235de4e949922b38ae820e8
Victors1967 <victorsmirnov@me.com>
2021-04-07 13:25:41 +0300

    222

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-const menuLinks = document.querySelectorAll('.menu-link');
+const navLinks = document.querySelectorAll('.nav-link');
 
-// menuLinks.forEach(item => {
-//     item.addEventListener('mouseover', event => {
-//         if (event.target.classList.contains('menu-item')) {
-//             event.target.style.color = 'red';
-//             event.target.style.fill = 'red';
-//         }
-//         if (event.target.classList.contains('menu-icon')) {
-//             event.target.style.fill = 'red';
-//         }
-//     })
-//     item.addEventListener('mouseout', event => {
-//         if (event.target.classList.contains('menu-item')) {
-//             event.target.style.color = '';
-//         }
-//         if (event.target.classList.contains('menu-icon')) {
-//             event.target.style.fill = '';
-//         }
-//     })
-// })
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', event => {
+        console.log(event.target);
+    })
+})
