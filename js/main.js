8e38f88650c922afac1ba7f99d467ee6898b3004
Victors1967 <victorsmirnov@me.com>
2021-04-07 10:45:44 +0300

    111

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const menuLinks = document.querySelectorAll('.menu-link');
+
+// menuLinks.forEach(item => {
+//     item.addEventListener('mouseover', event => {
+//         if (event.target.classList.contains('menu-item')) {
+//             event.target.style.color = 'red';
+//             event.target.style.fill = 'red';
+//         }
+//         if (event.target.classList.contains('menu-icon')) {
+//             event.target.style.fill = 'red';
+//         }
+//     })
+//     item.addEventListener('mouseout', event => {
+//         if (event.target.classList.contains('menu-item')) {
+//             event.target.style.color = '';
+//         }
+//         if (event.target.classList.contains('menu-icon')) {
+//             event.target.style.fill = '';
+//         }
+//     })
+// })
