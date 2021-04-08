dcf37005d7f834b67d12ff54a6504c954cae1432
Victors1967 <victorsmirnov@me.com>
2021-04-08 15:51:56 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 'auto',
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+    },
+});
