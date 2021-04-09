99ae07de71d6579a120c12ce44047ef2a31b56e8
Victors1967 <victorsmirnov@me.com>
2021-04-09 19:40:30 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 const swiper = new Swiper('.swiper-container', {
-    loop: true,
+    direction: 'vertical',
     slidesPerView: 'auto',
+    spaceBetween: 20,
     // Navigation arrows
     navigation: {
         nextEl: '.next',
         prevEl: '.prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        640: {
+            loop: true,
+            direction: 'horizontal',
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+        },        
+    },
 });
+
+const searchBtn = document.querySelector('.mobile-search'),
+        mobileSearch = document.querySelector('.search-group'),
+        body = document.querySelector('body');
+
+searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+});
+
+
